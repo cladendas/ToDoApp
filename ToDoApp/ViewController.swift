ea188c29a5eda2bc4b8812e6aea54e3bb7d154f0
cladendas <rvdagi05@yandex.ru>
2020-04-30 20:50:56 +0300

    Initial Commit

---
+++ ToDoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoApp
+//
+//  Created by cladendas on 30.04.2020.
+//  Copyright © 2020 cladendas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

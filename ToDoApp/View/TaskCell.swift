f32dcf7e17cb375b5076a3783868bc819d3e3c28
cladendas <rvdagi05@yandex.ru>
2020-05-02 21:07:32 +0300

    Наполнил DataProvider функционалом, создал тесты для него

---
+++ ToDoApp/View/TaskCell.swift
@@ -0,0 +1,15 @@
+//
+//  TaskCell.swift
+//  ToDoApp
+//
+//  Created by cladendas on 02.05.2020.
+//  Copyright © 2020 cladendas. All rights reserved.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+
+    
+
+}
